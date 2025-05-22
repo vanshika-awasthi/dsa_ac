7daa3e8cb98f0d658516f5782bc7cca2691636e5
Vanshika Awasthi <awasthi.vanshika21@gmail.com>
2025-05-22 09:33:32 +0530

    Swapping of numbers & Updated README.md

--- src/swap.java
+++ src/swap.java
@@ -1,3 +1,11 @@
+/* Lets understand swap concept with an Analogy, there are 3 boxes A, B and C.
+Rule is each box can hold only one item at a time.
+Box A and B each contains distinct item, whereas box C is empty. 
+We need to interchange the items of box A and B. Lets see how we can do this with the help of box C(introducing new varicable - temp).
+** 1. First we will put the item of box A into box C(in code - temp variable).
+** 2. Now we will put the item of box B into box A.
+** 3. Finally we will put the item of box C into box B. 
+*/ 
 public class swap{
     public static void main(String[] args){
         int a =5;
