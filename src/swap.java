61d6a817f5ab92aabacabd7b780a95ff6c84981b
Vanshika Awasthi <awasthi.vanshika21@gmail.com>
2025-05-22 09:21:14 +0530

    Swapping of numbers

---
+++ src/swap.java
@@ -0,0 +1,11 @@
+public class swap{
+    public static void main(String[] args){
+        int a =5;
+        int b = 6;
+        System.out.println("Before swap: a = " + a + ",b = " + b);
+        int temp = a;
+        a=b;
+        b=temp;
+        System.out.println("After swap: a = " + a + ",b = " + b);
+    }
+}
