490d59a969e33fe17c6d31418dc5d45c81628c45
Vanshika Awasthi <awasthi.vanshika21@gmail.com>
2025-05-22 08:47:27 +0530

    Added task2 - User input

---
+++ src/task2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Good Morning "+name);
+    }
+}
